510a614ae620bf14c36f004b2f218a4d94064a60
Oscar Bergqvist <obergq@kth.se>
2018-11-22 17:44:00 +0100

    the BIG problem is solved

--- main.m
+++ main.m
@@ -2,13 +2,15 @@ R = 0.004;
 L = 1e-6;
 C = 0.25e-8;
 N = 100;
-M = 100;
 X = 10000;
 t_max = 0.003;
-h_x = L / N;
-h_t = t_max / M; 
+h_x = X / N;
+% h_t = t_max / M; 
 a = 1/sqrt(L*C);
-sigma_adv = a * h_t / h_x;
+%sigma_adv = a * h_t / h_x;=
+sigma_adv=1; 
+h_t= h_x*sigma_adv/a;
+M =t_max/h_t;
 
 u = zeros(N+1, M+1);
 for k = 1:M+1
@@ -30,4 +32,14 @@ for k = 2:M
                 + 1/(2*L*C*h_x^2) * u(i-1, k));
     end
     u(N+1, k+1) = sigma_adv * u(N, k) + (1 - sigma_adv) * u(N+1, k);
+end
+
+%% PLOTS
+
+x_vec= 0:h_x:X;
+figure('Name', 'Animation of u')
+for k = 1:(M+1)
+   plot(x_vec, u(:, k))
+   axis([0,10000,0,1.1])
+   drawnow
 end
